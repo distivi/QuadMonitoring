3c532c75baf222f374629dfe9933c5192211dc34
Stanislav Dymedyuk <dymedyuk@gmail.com>
2015-12-17 19:34:30 +0200

    Initial Commit

---
+++ QuadMonitoring/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuadMonitoring
+//
+//  Created by Stanislav Dymedyuk on 12/17/15.
+//  Copyright © 2015 Stanislav Dymedyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuadMonitoring/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuadMonitoring
+//
+//  Created by Stanislav Dymedyuk on 12/17/15.
+//  Copyright © 2015 Stanislav Dymedyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
